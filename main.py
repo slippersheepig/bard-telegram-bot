c570acf43ca9a8bc0536d848d187c7777fd50069
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-04-03 21:24:49 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from os import environ
+import asyncio
+from telebot.async_telebot import AsyncTeleBot
+from Bard import Chatbot
+
+# get config
+token = environ.get("BARD_TOKEN")
+
+# init telegram bot
+bot_token = environ.get("TELEGRAM_BOT_TOKEN")
+bot = AsyncTeleBot(bot_token, parse_mode="MARKDOWN")
+
+# init chatbot
+chatbot = Chatbot(token)
+print("initial bot...")
+
+# define a message handler to send a message when the command /start is issued
+@bot.message_handler(commands=["start", "hello"])
+async def send_welcome(message):
+    await bot.reply_to(message, "This bot uses the google bard")
+
+@bot.message_handler(func=lambda m: True)
+async def send_gpt(message):
+    print("get response...")
+    await bot.send_chat_action(message.chat.id, 'typing')
+#   await bot.send_message(message.chat.id, "思考中，请稍后")
+    response = chatbot.ask(message.text)
+    await bot.reply_to(message, response["content"])
+
+
+# run the bot
+asyncio.run(bot.polling())
