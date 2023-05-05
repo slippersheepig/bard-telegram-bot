d80c054b1d23d8a8076bd29afd67c8b1c1f321df
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-05-06 00:28:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from os import environ
 import asyncio
 from telebot.async_telebot import AsyncTeleBot
-from Bard import Chatbot
+from bard import Chatbot
 
 # get config
 token = environ.get("BARD_TOKEN")
@@ -17,27 +17,28 @@ print("initial bot...")
 # define a message handler to send a message when the command /start is issued
 @bot.message_handler(commands=["start", "hello"])
 async def send_welcome(message):
-    await bot.reply_to(message, "This bot uses the google bard")
+    await bot.reply_to(message, "This bot uses the Google BARD")
     
 # define an async function to continuously display the input state
-async def display_typing(chat_id):
-    while True:
+async def display_typing(chat_id, duration=3):
+    for _ in range(duration):
         await bot.send_chat_action(chat_id, 'typing')
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(1)
 
 # define a message handler to send the response and start the input state display
 @bot.message_handler(func=lambda m: True)
 async def send_gpt(message):
     print("get response...")
     try:
-        #start the input state display
-        typing_task = asyncio.create_task(display_typing(message.chat.id))
+        # start the input state display
+        typing_duration = 5 # increased the duration to 5 seconds
+        typing_task = asyncio.create_task(display_typing(message.chat.id, typing_duration))
         response = chatbot.ask(message.text)
-        #stop the input state display and send the response
+        # stop the input state display and send the response
         typing_task.cancel()
-        await bot.reply_to(message, response["content"])
+        await bot.reply_to(message, response.content)
     except Exception as e:
-        await bot.reply_to(message, e)
+        await bot.reply_to(message, str(e))
 
 # run the bot
-asyncio.run(bot.polling())
+bot.run()
