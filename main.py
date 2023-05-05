4c74272676aa38257e7679c4a98f7ad7709054cd
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-05-05 23:16:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,14 +18,23 @@ print("initial bot...")
 @bot.message_handler(commands=["start", "hello"])
 async def send_welcome(message):
     await bot.reply_to(message, "This bot uses the google bard")
+    
+# define an async function to continuously display the input state
+async def display_typing(chat_id):
+    while True:
+        await bot.send_chat_action(chat_id, 'typing')
+        await asyncio.sleep(1)
 
+# define a message handler to send the response and start the input state display
 @bot.message_handler(func=lambda m: True)
 async def send_gpt(message):
     print("get response...")
     try:
-        await bot.send_chat_action(message.chat.id, 'typing')
-#        await bot.send_message(message.chat.id, "思考中，请稍后")
+        #start the input state display
+        typing_task = asyncio.create_task(display_typing(message.chat.id))
         response = chatbot.ask(message.text)
+        #stop the input state display and send the response
+        typing_task.cancel()
         await bot.reply_to(message, response["content"])
     except Exception as e:
         await bot.reply_to(message, e)
