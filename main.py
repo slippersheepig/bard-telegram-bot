b8336b90a4e72de40dded500117057b7fefc3b53
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-07-21 12:53:34 +0800

    another fix

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 from os import environ
 import asyncio
-from telebot.async_telebot import AsyncTeleBot
+import telebot
 from Bard import Chatbot
 
 # get config
 Secure_1PSID = environ.get("BARD__Secure_1PSID")
 Secure_1PSIDTS = environ.get("BARD__Secure_1PSIDTS")
+chatbot = Chatbot(Secure_1PSID, Secure_1PSIDTS)
 
 # init telegram bot
 bot_token = environ.get("TELEGRAM_BOT_TOKEN")
-bot = AsyncTeleBot(bot_token, parse_mode="HTML")
+bot = telebot.TeleBot(bot_token, parse_mode="HTML")
 
 # define a message handler to send a message when the command /start is issued
 @bot.message_handler(commands=["start", "hello"])
-async def send_welcome(message):
-    await bot.reply_to(message, "This bot uses the google bard")
+def send_welcome(message):
+    bot.reply_to(message, "This bot uses the google bard")
 
 @bot.message_handler(func=lambda m: True)
-async def send_gpt(message):
-    chatbot = AsyncChatbot(Secure_1PSID, Secure_1PSIDTS)
+def send_gpt(message):
     print("get response...")
     try:
-        await bot.send_chat_action(message.chat.id, 'typing')
-#        await bot.send_message(message.chat.id, "思考中，请稍后")
-        response = await chatbot.ask(message.text)
-        await bot.reply_to(message, response["content"])
+        bot.send_chat_action(message.chat.id, 'typing')
+        response = chatbot.ask(message.text)
+        bot.reply_to(message, response["content"])
     except BaseException as e:
-        await bot.reply_to(message, str(e))
+        bot.reply_to(message, str(e))
 
 # run the bot
 asyncio.run(bot.polling())
