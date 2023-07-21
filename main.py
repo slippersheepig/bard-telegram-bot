682ccb4effb6d838428e2a42615a139d62bb6d97
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-07-21 10:26:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from os import environ
-import nest_asyncio
+import asyncio
 from telebot.async_telebot import AsyncTeleBot
-from Bard import Chatbot
+from Bard import AsyncChatbot
 
 # get config
 Secure_1PSID = environ.get("BARD__Secure_1PSID")
@@ -11,10 +11,6 @@ Secure_1PSIDTS = environ.get("BARD__Secure_1PSIDTS")
 bot_token = environ.get("TELEGRAM_BOT_TOKEN")
 bot = AsyncTeleBot(bot_token, parse_mode="MARKDOWN")
 
-# init chatbot
-chatbot = Chatbot(Secure_1PSID, Secure_1PSIDTS)
-print("initial bot...")
-
 # define a message handler to send a message when the command /start is issued
 @bot.message_handler(commands=["start", "hello"])
 async def send_welcome(message):
@@ -22,14 +18,15 @@ async def send_welcome(message):
 
 @bot.message_handler(func=lambda m: True)
 async def send_gpt(message):
+    chatbot = await AsyncChatbot(Secure_1PSID, Secure_1PSIDTS)
     print("get response...")
     try:
         await bot.send_chat_action(message.chat.id, 'typing')
 #        await bot.send_message(message.chat.id, "思考中，请稍后")
-        response = chatbot.ask(message.text)
+        response = await chatbot.ask(message.text)
         await bot.reply_to(message, response["content"])
     except BaseException as e:
         await bot.reply_to(message, str(e))
 
 # run the bot
-nest_asyncio.apply()
+asyncio.run(bot.polling())
